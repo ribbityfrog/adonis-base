38bfa68f12621279237ff4760df84629dceb56f8
LeX <pierre@lastexile.fr>
2025-02-01 10:24:36 +0100

    Correct routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,21 +8,9 @@
 */
 
 import router from '@adonisjs/core/services/router'
-import { middleware } from './kernel.js'
-
-// Routes
-import AccountsRoutes from '#routes/accounts'
-
-router.get('/', () => 'Hello :)')
-router.get('/favicon.ico', () => 'Not a website ^^')
 
 const sandboxesController = () => import('#controllers/sandboxes_controller')
 
-router.get('/sand', [sandboxesController, 'sand'])
-router.get('/sandGuarded', [sandboxesController, 'sand']).use(middleware.auth({ guards: ['api'] }))
-router
-  .get('/sandAdmin', [sandboxesController, 'sand'])
-  .use(middleware.auth({ guards: ['api'] }))
-  .use(middleware.admin())
-
-AccountsRoutes()
+router.get('/mars', [sandboxesController, 'mars'])
+router.get('/inaric', [sandboxesController, 'inaric'])
+router.get('/tesseract', [sandboxesController, 'tesseract'])
